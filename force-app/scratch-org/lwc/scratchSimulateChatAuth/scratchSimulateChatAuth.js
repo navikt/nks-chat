5ab5d0b89722741d445e2df61f6280fbd366ff14
SRS <stian.schikora@nav.no>
2022-03-02 11:53:19 +0100

    Scratch dummy user

--- force-app/scratch-org/lwc/scratchSimulateChatAuth/scratchSimulateChatAuth.js
+++ force-app/scratch-org/lwc/scratchSimulateChatAuth/scratchSimulateChatAuth.js
@@ -2,16 +2,25 @@ import { LightningElement } from 'lwc';
 import setStatusCompleted from '@salesforce/apex/ChatAuthController.setStatusCompleted';
 
 export default class ScratchSimulateChatAuth extends LightningElement {
+    loading = false;
+    errorMsg;
+
     initAuth() {
         let chatId = this.inputField.value;
         console.log('INIT AUTH: ' + chatId);
         if (chatId && chatId != '') {
+            this.loading = true;
+            this.errorMsg = null;
             setStatusCompleted({ chatTranscriptId: chatId })
                 .then((result) => {
                     console.log(result);
                 })
                 .catch((error) => {
                     console.log(error);
+                    this.errorMsg = error.body.message;
+                })
+                .finally(() => {
+                    this.loading = false;
                 });
         }
     }
