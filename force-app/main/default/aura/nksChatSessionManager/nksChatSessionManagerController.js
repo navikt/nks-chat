fb4969694d8c6f452cca51280aa345b167503aaa
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2023-02-23 23:50:06 +0100

    updates

--- force-app/main/default/aura/nksChatSessionManager/nksChatSessionManagerController.js
+++ force-app/main/default/aura/nksChatSessionManager/nksChatSessionManagerController.js
@@ -2,12 +2,37 @@
     onAgentSend: function (cmp, evt, helper) {
         var content = evt.getParam('content');
         var type = evt.getParam('type');
-
+        var message = 'Gi oss tilbakemlding ved å trykke på denne lenken:\n';
         if (content.toLowerCase().includes('mvh') && type === 'Agent') {
-            var recordId = cmp.get('v.recordId');
-            var sessionIdShort =
-                recordId != null && recordId != '' && recordId.length > 15 ? recordId.substring(0, 15) : recordId;
-            helper.sendMessage(cmp, evt, sessionIdShort);
+            try {
+                var action = cmp.get('c.createSurveyInvitation');
+                action.setParams({ caseId: cmp.get('v.record.CaseId') });
+                action.setCallback(this, function (response) {
+                    var state = response.getState();
+                    console.log(response.getReturnValue());
+                    if (state === 'SUCCESS' && response.getReturnValue().includes('https')) {
+                        message += response.getReturnValue();
+                        var recordId = cmp.get('v.recordId');
+                        var sessionIdShort =
+                            recordId != null && recordId != '' && recordId.length > 15
+                                ? recordId.substring(0, 15)
+                                : recordId;
+                        helper.sendMessage(cmp, sessionIdShort, message);
+                    } else if (state === 'ERROR') {
+                        var errors = response.getError();
+                        if (errors) {
+                            if (errors[0] && errors[0].message) {
+                                console.log('Error message: ' + errors[0].message);
+                            }
+                        } else {
+                            console.log('Unknown error!');
+                        }
+                    }
+                });
+                $A.enqueueAction(action);
+            } catch (error) {
+                console.log('Problem getting Survey Link: ' + error);
+            }
         }
     }
 });
--- force-app/main/default/aura/nksChatSessionManager/nksChatSessionManagerHelper.js
+++ force-app/main/default/aura/nksChatSessionManager/nksChatSessionManagerHelper.js
@@ -1,32 +1,21 @@
 ({
-    sendMessage: function (cmp, evt, sessionIdShort) {
+    sendMessage: function (cmp, sessionIdShort, message) {
+        var conversationKit = cmp.find('conversationKit');
         if (sessionIdShort != null && sessionIdShort != '') {
-            var conversationKit = cmp.find('conversationKit');
-            var action = cmp.get('c.createSurveyInvitation');
-            console.log(cmp.get('v.record.CaseId'));
-            action.setParam({ caseId: cmp.get('v.record.CaseId') });
-            action.setCallback(this, function (response) {
-                var state = response.getState();
-                if (state === 'SUCCESS') {
-                    conversationKit
-                        .sendMessage({
-                            recordId: sessionIdShort,
-                            message: {
-                                text: response.getReturnValue()
-                            }
-                        })
-                        .then(function (result) {
-                            if (result) {
-                                console.log('Successfully sent message!');
-                            } else {
-                                console.log('Failed to send message!');
-                            }
-                        });
-                } else {
-                    console.log('Problem getting Survey link: ' + response.getReturnValue());
-                }
-            });
-            $A.enqueueAction(action);
+            conversationKit
+                .sendMessage({
+                    recordId: sessionIdShort,
+                    message: {
+                        text: message
+                    }
+                })
+                .then(function (result) {
+                    if (result) {
+                        console.log('Successfully sent message: ' + message);
+                    } else {
+                        console.log('Failed to send message!');
+                    }
+                });
         }
     }
 });
