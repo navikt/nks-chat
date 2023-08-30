9c97073d5f5837a397d918b488bbc69dd94950d1
Morten Mikalsen <morten.mikalsen@hotmail.com>
2023-08-30 14:31:31 +0200

    Adds beta check to afterwork

--- force-app/main/default/aura/nksChatAfterWork/nksChatAfterWorkController.js
+++ force-app/main/default/aura/nksChatAfterWork/nksChatAfterWorkController.js
@@ -1,8 +1,35 @@
 ({
+    doInit: function (component, event, helper) {
+        console.log('Very poggers aa');
+        var action = component.get('c.hasBetaAccess');
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            console.log(state);
+            if (state === 'SUCCESS') {
+                component.set('v.betaAccess', response.getReturnValue());
+
+                // You would typically fire a event here to trigger
+                // client-side notification that the server-side
+                // action is complete
+            } else if (state === 'INCOMPLETE') {
+                // do something
+            } else if (state === 'ERROR') {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log('Error message: ' + errors[0].message);
+                    }
+                } else {
+                    console.log('Unknown error');
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
     handleChatEnded: function (component, event, helper) {
         var recordId = component.get('v.recordId');
         var eventRecordId = event.getParam('recordId');
-        if (recordId === eventRecordId) {
+        if (recordId === eventRecordId && component.get('v.betaAccess')) {
             helper.startTimer(component, eventRecordId);
         }
     }
